c03ee5025845929608fbe923b3214a1872a6e089
AShields428 <ashields@ucsb.edu>
2020-10-22 15:04:44 -0700

    Thomas Fire Progression and Intro R markdown update

--- Output_Data/Week 1/ThomasFire_Progression.R
+++ Output_Data/Week 1/ThomasFire_Progression.R
@@ -5,20 +5,41 @@
 
 ##### Packages #####
 
-install.packages("lubridate")
+#install.packages("lubridate")
 library(lubridate)
 
-install.packages("tidyverse")
+#install.packages("tidyverse")
 library(tidyverse)
 
-install.packages("readxl")
+#install.packages("readxl")
 library(readxl)
 
-install.packages("dplyr")
+#install.packages("dplyr")
 library(dplyr)
 
 excel_sheets("Input_Data/week1/Thomas_Fire_Progression.xlsx")
 
 Thomasfire.data <- read_excel("Input_Data/week1/Thomas_Fire_Progression.xlsx", sheet = "Data")
 
-Thomasfire.data <- read_excel("Input_Data/week1/Thomas_Fire_Progression.xlsx", sheet = "Metadata")
+#Thomasfire.data <- read_excel("Input_Data/week1/Thomas_Fire_Progression.xlsx", sheet = "Metadata")
+
+##### Data Exploration #####
+
+#names(Thomasfire.data)
+#head(Thomasfire.data)
+#tail(Thomasfire.data)
+#glimpse(Thomasfire.data)
+
+ThomasFire <- Thomasfire.data %>% 
+  arrange(desc(Acres_Burned)) %>%
+  mutate_at(vars( "PM10"), replace_na, 0) %>%
+  #mutate(dur = as.duration(interv),
+         #days = as.numeric(dur, "days")) %>% 
+
+##### ggplot #####
+
+ggplot(ThomasFire, aes(x = Date, y = Acres_Burned))+
+ geom_point() %>%
+  
+
+

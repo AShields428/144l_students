4dfe5b29345e1fcef2f27c7d13437c01ee18954f
AShields428 <ashields@ucsb.edu>
2020-10-22 18:38:44 -0700

    ggplot update

--- Output_Data/Week 1/ThomasFire_Progression.R
+++ Output_Data/Week 1/ThomasFire_Progression.R
@@ -33,12 +33,12 @@ Thomasfire.data <- read_excel("Input_Data/week1/Thomas_Fire_Progression.xlsx", s
 ThomasFire <- Thomasfire.data %>% 
   arrange(desc(Acres_Burned)) %>%
   mutate_at(vars( "PM10"), replace_na, 0) %>%
-  #mutate(dur = as.duration(interv),
-         #days = as.numeric(dur, "days")) %>% 
+  mutate(dur = as.duration(interv),
+         days = as.numeric(dur, "days")) %>% 
 
 ##### ggplot #####
 
-ggplot(ThomasFire, aes(x = Date, y = Acres_Burned))+
+ggplot(ThomasFire, aes(x = Date, y = Acres_Burned)) +
  geom_point() %>%
   
 
